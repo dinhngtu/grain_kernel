960313238608f78ad5a5820876f0c36ead4929a6
Tu Dinh <dinhngtu@users.noreply.github.com>
2020-01-21 02:12:02 +0100

    add multiboot2 memory map

--- src/arch/x86_64/boot.rs
+++ src/arch/x86_64/boot.rs
@@ -3,11 +3,34 @@ use crate::arch::serial::COM1;
 use core::ffi::c_void;
 use core::fmt::Write;
 
+fn memory_type(num: u32) -> &'static str {
+    match num {
+        x if x == MemoryMapType::RAM as u32 => "RAM",
+        x if x == MemoryMapType::ACPI as u32 => "ACPI",
+        x if x == MemoryMapType::HibernationReserved as u32 => "HibernationReserved",
+        x if x == MemoryMapType::BadMemory as u32 => "BadMemory",
+        _ => "Reserved",
+    }
+}
+
 #[no_mangle]
 pub extern "sysv64" fn x86_64_start(ptr: *const c_void) -> ! {
     for tag in BootInfoReader::from(ptr) {
         match tag {
             BootInfoTag::Cmdline(s) => writeln!(*COM1.lock(), "Command line: {}", s).unwrap(),
+            BootInfoTag::Mmap(maps) => {
+                for map in maps {
+                    writeln!(
+                        *COM1.lock(),
+                        "Memory map: base_addr {:#x}, length {:#x}, map_type {} ({})",
+                        map.base_addr,
+                        map.length,
+                        map.map_type,
+                        memory_type(map.map_type)
+                    )
+                    .unwrap();
+                }
+            }
             BootInfoTag::Unknown => {}
             _ => {}
         };
--- src/arch/x86_64/multiboot2.rs
+++ src/arch/x86_64/multiboot2.rs
@@ -1,13 +1,13 @@
 use crate::arch::serial::COM1;
-use core::convert::From;
+use core::convert::{From, TryInto};
 use core::ffi::c_void;
 use core::fmt::Write;
 use core::mem::size_of;
-use core::slice::from_raw_parts;
 
 #[derive(Copy, Clone, PartialEq)]
 #[repr(u32)]
 #[allow(dead_code)]
+#[non_exhaustive]
 enum BootInfoTagType {
     End = 0,
     Cmdline,
@@ -33,21 +33,40 @@ enum BootInfoTagType {
     LoadBaseAddr,
 }
 
-#[repr(C, packed)]
+#[repr(C)]
 struct BootInfoHeader {
     total_size: u32,
     _reserved: u32,
 }
 
-#[repr(C, packed)]
+#[repr(C)]
 struct BootInfoTagBase {
-    tag_type: BootInfoTagType,
+    tag_type: u32,
     size: u32,
 }
 
 pub struct BootInfoReader<'a> {
     buffer: &'a [u8],
     offset: usize,
+}
+
+#[derive(Copy, Clone, PartialEq)]
+#[repr(u32)]
+#[allow(dead_code)]
+#[non_exhaustive]
+pub enum MemoryMapType {
+    RAM = 1,
+    ACPI = 3,
+    HibernationReserved = 4,
+    BadMemory = 5,
+}
+
+#[repr(C)]
+pub struct MemoryMapEntry {
+    pub base_addr: u64,
+    pub length: u64,
+    pub map_type: u32,
+    pub reserved: u32,
 }
 
 #[allow(dead_code)]
@@ -57,7 +76,7 @@ pub enum BootInfoTag<'a> {
     Module,
     BasicMeminfo,
     Bootdev,
-    Mmap,
+    Mmap(&'a [MemoryMapEntry]),
     Vbe,
     Framebuffer,
     ElfSections,
@@ -86,20 +105,29 @@ impl<'a> From<*const c_void> for BootInfoReader<'a> {
         if (header.total_size as usize) < MBI2_HEADER_LEN {
             panic!("invalid mbi2 header size");
         }
-        unsafe {
-            writeln!(
-                *COM1.lock(),
-                "header total_size {} reserved {}",
-                header.total_size,
-                header._reserved
-            )
-            .unwrap();
-        }
-        let total_buffer = unsafe { from_raw_parts(ptr.cast::<u8>(), header.total_size as usize) };
+        writeln!(
+            *COM1.lock(),
+            "header total_size {} reserved {}",
+            header.total_size,
+            header._reserved
+        )
+        .unwrap();
+        let total_buffer =
+            unsafe { core::slice::from_raw_parts(ptr.cast::<u8>(), header.total_size as usize) };
         BootInfoReader {
             buffer: total_buffer.get(MBI2_HEADER_LEN..).unwrap(),
             offset: 0,
         }
+    }
+}
+
+fn from_ne_bytes(buf: &[u8]) -> Option<u32> {
+    if buf.len() != 4 {
+        return None;
+    }
+    match TryInto::<[u8; 4]>::try_into(buf) {
+        Ok(bytes) => Some(u32::from_ne_bytes(bytes)),
+        _ => None,
     }
 }
 
@@ -118,7 +146,7 @@ impl<'a> Iterator for BootInfoReader<'a> {
                 .map(|x| x.cast::<BootInfoTagBase>())
                 .and_then(|x| unsafe { x.as_ref() })
                 .unwrap();
-            unsafe {
+            {
                 writeln!(
                     *COM1.lock(),
                     "tag type {} size {}",
@@ -133,11 +161,35 @@ impl<'a> Iterator for BootInfoReader<'a> {
                 .get((self.offset + MBI2_TAG_BASE_LEN)..(self.offset + tag_base.size as usize))
                 .unwrap();
             let tag = match tag_base.tag_type {
-                BootInfoTagType::Cmdline => {
+                x if x == BootInfoTagType::Cmdline as u32 => {
                     let cmdline = core::str::from_utf8(tag_data).unwrap();
                     Some(BootInfoTag::Cmdline(cmdline))
                 }
-                BootInfoTagType::End => None,
+                x if x == BootInfoTagType::Mmap as u32 => {
+                    let entry_size: u32 = tag_data.get(0..4).and_then(from_ne_bytes).unwrap();
+                    let entry_version: u32 = tag_data.get(4..8).and_then(from_ne_bytes).unwrap();
+                    writeln!(
+                        *COM1.lock(),
+                        "memory map size {} version {}",
+                        entry_size,
+                        entry_version
+                    )
+                    .unwrap();
+                    if entry_size != 24 || entry_version != 0 {
+                        panic!("unsupported memory map version");
+                    }
+                    if (tag_data.len() - 8) % (entry_size as usize) != 0 {
+                        panic!("invalid memory map count");
+                    }
+                    let maps = unsafe {
+                        core::slice::from_raw_parts(
+                            tag_data.get(8..)?.as_ptr().cast::<MemoryMapEntry>(),
+                            (tag_data.len() - 8) / (entry_size as usize),
+                        )
+                    };
+                    Some(BootInfoTag::Mmap(maps))
+                }
+                x if x == BootInfoTagType::End as u32 => None,
                 _ => Some(BootInfoTag::Unknown),
             };
             self.offset += tag_base.size as usize;
