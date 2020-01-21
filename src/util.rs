90f82ac5cfa3c4bc908d1f6cf2b3a33d135685ca
Tu Dinh <dinhngtu@users.noreply.github.com>
2020-01-21 14:48:19 +0100

    small change to from_bytes

--- src/util.rs
+++ src/util.rs
@@ -1,13 +1,15 @@
 use core::mem::size_of;
 
 pub unsafe fn from_bytes<'a, T>(bytes: &'a [u8]) -> Option<&'a T> {
-    match bytes.len() {
-        x if x == size_of::<T>() => Some(&*(bytes.as_ptr() as *const T)),
-        _ => None,
+    if bytes.len() == size_of::<T>() {
+        Some(&*(bytes.as_ptr() as *const T))
+    } else {
+        None
     }
 }
 
 pub unsafe fn slice_from_bytes<'a, T>(bytes: &'a [u8]) -> Option<&'a [T]> {
+    assert_ne!(size_of::<T>(), 0);
     if bytes.len() % size_of::<T>() == 0 {
         let nr = bytes.len() / size_of::<T>();
         Some(core::slice::from_raw_parts(bytes.as_ptr() as *const T, nr))
