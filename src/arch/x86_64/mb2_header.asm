4375043ae585af63a5821ac28b86bbfcd4884bfc
brice <brice.ekane@gmail.com>
2020-01-26 17:48:55 +0100

    Added usefull comments

--- src/arch/x86_64/mb2_header.asm
+++ src/arch/x86_64/mb2_header.asm
@@ -1,9 +1,26 @@
 section .multiboot2
+
+;Offset , Type , Field         , Name     , Note
+;0      , u32i , magic         , required
+;4      , u32  , architecture  , required
+;8      , u32  , header_length , required
+;12     , u32  , checksum      , required
+;16-XX  , tags , required
+
 mb2_header_start:
     dd 0xe85250d6
-    dd 0
+    dd 0 ; protected mode code grub will automatocally do the transition from 16bits to 32bits/A20 line
     dd mb2_header_end - mb2_header_start
     dd 0x100000000 - (0xe85250d6 + 0 + (mb2_header_end - mb2_header_start))
+
+;        Tags
+;        +-------------------+
+;u16     | type              |
+;u16     | flags             |
+;u32     | size              |
+;        +-------------------+
+
+; we ignore unusable tags from now
 
     ;dw 7  ; efi services tag
     ;dw 0
@@ -14,8 +31,8 @@ mb2_header_start:
     ;dd 12
     ;dd 0  ; addr = 0
 
+; required, grub failed to load if end tag not present
     dw 0  ; end tag
     dw 0
     dd 8
-
 mb2_header_end:
